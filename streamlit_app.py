1b663529b80d9c048cbe087ed5d7f7cc92fd88b2
Zain Akhtar <115431171+xzxShadowZxzx@users.noreply.github.com>
2026-04-22 14:05:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,6 @@ if ingredient_list:
         st.success('Your Smoothie is ordered '+ name_on_order + '!', icon="✅")
 
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
