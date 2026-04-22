5b862d4eeef0db72d22816db23c69eca70c31077
Zain Akhtar <115431171+xzxShadowZxzx@users.noreply.github.com>
2026-04-22 14:01:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ if ingredient_list:
 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
